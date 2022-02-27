3e48ace665273f74c1c6388101ed508b55184382
DiamondStrider1 <62265561+Swift-Strider@users.noreply.github.com>
2022-02-27 04:29:35 -0600

    add SoundIds

---
+++ tools/generate_sounds.php
@@ -0,0 +1,94 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Converts the sound name (ex: step.stem) to a const name (ex: STEP_STEM).
+ */
+function soundNameToConstCase(string $name): string
+{
+    return strtoupper(str_replace('.', "_", $name));
+}
+
+/**
+ * Used to convert file paths to corresponding sound names.
+ *
+ * ```php
+ * $file = "sounds/step/wood6.fsb";
+ * $name = slashToPeriods($file);
+ * assert($name === "step.wood6");
+ * ```
+ */
+function slashToPeriods(string $name): string {
+    $name = preg_replace('/.*sounds\//', "", $name);
+    $name = preg_replace('/\..*$/', "", $name);
+    return preg_replace('/\//', ".", $name);
+}
+
+/**
+ * A generator over all the sound names in pack.zip
+ *
+ * @return string[]
+ */
+function soundNames(): array {
+    $zip = new ZipArchive;
+    $zip->open(__DIR__ . "/../pack.zip");
+
+    // Many files can correspond to the same sound.
+    // Files here won't get ignored for having a number
+    $exceptions = [
+        "random.pop2" => true,
+        "item.trident.riptide_1" => true,
+        "item.trident.riptide_2" => true,
+        "item.trident.riptide_3" => true,
+        "record.11" => true,
+        "record.13" => true,
+    ];
+
+    $return = [];
+    $files = $zip->count();
+    for ($i = 0; $i < $files; $i++) {
+        $name = $zip->getNameIndex($i);
+        if (preg_match("/sounds\/.*\./", $name)) {
+            $name = slashToPeriods($name);
+            // Check if this sound has many files
+            if (!isset($exceptions[$name])
+                && preg_match('/\d$/', $name)
+            ) {
+                if (!preg_match('/1^/', $name)) {
+                    continue;
+                }
+                // This strips the "1". Ex: step.stem1 -> step.stem
+                $name = substr($name, 0, strlen($name) - 1);
+            }
+            $return[] = $name;
+        }
+    }
+    return $return;
+}
+
+$soundIds = fopen(__DIR__ . "/../src/DiamondStrider1/Sounds/SoundIds.php", "w");
+fwrite($soundIds, <<<EOT
+<?php
+
+declare(strict_types=1);
+
+namespace DiamondStrider1\Sounds;
+
+final class SoundIds
+{
+
+EOT);
+foreach (soundNames() as $sound) {
+    $constSound = soundNameToConstCase($sound);
+    fwrite($soundIds, <<<EOT
+        public const $constSound = "$sound";
+
+    EOT);
+}
+fwrite($soundIds, <<<EOT
+}
+
+EOT);
+fclose($soundIds);
+
